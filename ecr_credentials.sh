47fc6a5d825695cb5ca83100a0dae6e506b7bf85
Arnulfo <arnulfo.duran@wrethink.com>
2020-01-29 00:06:51 -0800

    .travis modified

--- ecr_credentials.sh
+++ ecr_credentials.sh
@@ -4,6 +4,6 @@ mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOL
 [amplifyProfile]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+aws_access_key_id = ${AWS_ACCESS_KEY}
+aws_secret_access_key = ${AWS_SECRET_KEY}
 EOL
