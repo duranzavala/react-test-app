e59cbc686dd18b11609af3323274f7f17d15fd32
Arnulfo <arnulfo.duran@wrethink.com>
2020-01-30 15:16:31 -0800

    .travis and ecr_credentials modified

--- ecr_credentials.sh
+++ ecr_credentials.sh
@@ -3,12 +3,12 @@
 mkdir -p ~/.aws
 
 cat > ~/.aws/config << EOL
-[default]
+[profile amplifyProfile]
 region=us-west-2
 EOL
 
 cat > ~/.aws/credentials << EOL
-[default]
+[amplifyProfile]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
