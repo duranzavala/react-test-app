b1b5929a8a687fdd857f5c43b627e274dc5a4980
Arnulfo <arnulfo.duran@wrethink.com>
2020-01-30 14:53:34 -0800

    Add configs to .travis

--- ecr_credentials.sh
+++ ecr_credentials.sh
@@ -2,8 +2,13 @@
 
 mkdir -p ~/.aws
 
+cat > ~/.aws/config << EOL
+[profile amplifyProfile]
+region=us-west-2
+EOL
+
 cat > ~/.aws/credentials << EOL
 [amplifyProfile]
-aws_access_key_id = ${AWS_ACCESS_KEY}
-aws_secret_access_key = ${AWS_SECRET_KEY}
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
