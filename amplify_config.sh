fe60f884e4400848a1cb6ff7cc04286c9219234f
Arnulfo <arnulfo.duran@wrethink.com>
2020-01-30 22:05:38 -0800

    amplify script configure added and .travis.yml modified

---
+++ amplify_config.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+IFS='|'
+
+REACTCONFIG="{\
+\"SourceDir\":\"src\",\
+\"DistributionDir\":\"build\",\
+\"BuildCommand\":\"npm run-script build\",\
+\"StartCommand\":\"npm run-script start\"\
+}"
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":true,\
+\"profileName\":${AWS_PROFILE},\
+\"accessKeyId\":${AWS_ACCESS_KEY_ID},\
+\"secretAccessKey\":${AWS_SECRET_ACCESS_KEY},\
+\"region\":${REGION}\
+}"
+AMPLIFY="{\
+\"projectName\":\"test_app\",\
+\"envName\":\"dev\",\
+\"defaultEditor\":\"vscode\"\
+}"
+FRONTEND="{\
+\"frontend\":\"javascript\",\
+\"framework\":\"react\",\
+\"config\":$REACTCONFIG\
+}"
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
+
+amplify pull --appId ${APP_ID} --envName dev \
+--amplify $AMPLIFY \
+--frontend $FRONTEND \
+--providers $PROVIDERS \
+--yes
