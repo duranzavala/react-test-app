aeb5cd6a138cabcaf51fd758b7f041b176612a80
Arnulfo <arnulfo.duran@wrethink.com>
2020-01-30 22:23:01 -0800

    amplify_config.sh modified

--- ecr_credentials.sh
+++ ecr_credentials.sh
@@ -13,6 +13,6 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
 
-echo $AWS_PROFILE
-echo $REGION
-echo $APP_ID
+echo ${AWS_PROFILE}
+echo ${REGION}
+echo ${APP_ID}
